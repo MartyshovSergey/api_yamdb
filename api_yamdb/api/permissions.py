eca0117c7ea059ca7b33bca8ba3fcbb6b81ad047
Klikovskiy <8Yx~q}F1AsxE>
2022-11-07 15:55:24 +0700

    правки PEP8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,16 +3,10 @@ from rest_framework import permissions
 
 class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.user.is_admin
-                or request.user.is_staff
-        )
+        return request.user.is_admin or request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.user.is_admin
-                or request.user.is_staff
-        )
+        return request.user.is_admin or request.user.is_staff
 
 
 class IsAdminUserOrReadOnly(permissions.BasePermission):
@@ -26,15 +20,11 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
 
 class IsAdminAuthorModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.is_moderator
-                or request.user.is_admin
-        )
+                or request.user.is_admin)
